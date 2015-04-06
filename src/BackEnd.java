c9dce7d2c138b315daffe979a61f033bcedf66b5
jacksongray <jacksoncharlesgray@gmail.com>
2015-04-06 15:42:04 -0500

    Methods BackEnd

--- src/BackEnd.java
+++ src/BackEnd.java
@@ -1,6 +1,24 @@
+import java.util.List;
 
-public class BackEnd {
+
+public class BackEnd 
+{
 	
 	//jackson
+	
+	public List<String> getMessages(String username)
+	{
+		return null;
+	}
+	
+	public void addMessage(String username, String msg)
+	{
+		
+	}
 
+	public List<String> getUsers()
+	{
+		return null;
+	}
+	
 }
