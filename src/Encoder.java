548aa4562ca7f099659b30de410eb6473223544d
quancung00 <quancung00@gmail.com>
2015-04-10 14:54:44 -0500

    Finished Encoder

--- src/Encoder.java
+++ src/Encoder.java
@@ -1,113 +1,41 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.List;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Base64;
 
-public class Encoder 
-{
-	private String filename;
-	private FileWriter fileWriter;
-	private FileReader fileReader;
-   
-   public Encoder(String filename)
-   {
-	   //STEPS
-	   //1. Create a FileWriter (which creates a file in the process)
-	   //2. Create a FileReader (which can read from a file)
-	   
-	   try
-	   {
-		   fileWriter = new FileWriter(filename);
-	   }
-	   catch (IOException e)
-	   {
-		   // TODO Display Catch exception
-	   }
-	   try
-	   {
-		  fileReader = new FileReader(filename);
-	   }
-	   catch(FileNotFoundException e)
-	   {
-		   // TODO Display Catch Exception
-	   }
-	   
-   }
-   public void write(String plaintext)
-   {
-	   
-   }
-   
-   public List<String> read()
-   {
-	   return null;
-   }
+public class Encoder {
 	
-	/*public static void main(String[] args) {
-      String message;  // Line of text entered by user.
-      Scanner sc = new Scanner(System.in);
-      System.out.println("Enter the message: ");
-      message = sc.nextLine();
-      String codeWord;
-      System.out.println("Enter the code word: ");
-      codeWord = sc.nextLine();
-      System.out.println("(E)ncode or (D)ecode? ");
-      String encode = sc.nextLine();
-      if (encode.equals("E") || encode.equals("e") )
-        System.out.println(encode(message, codeWord, true));
-      else
-        System.out.println(encode(message, codeWord, false));
-         
-       
-    
-   }
-    
-   static String encode(String msg, String code, boolean encode) {
-       char cha;
-       String message2 = Capitalize(msg);
-       String message3= "";
-       boolean enc = encode;
-       String cw = code;
-       int [] codeWord = new int[cw.length()];
-       for (int ind = 0; ind < cw.length(); ind++)
-            codeWord[ind] = (int)(cw.charAt(ind))- (int)('A') + 1;
-       int ctr = 0;
-           for (int ind = 0; ind < message2.length(); ind++) {
-               cha = message2.charAt(ind);
-               if (enc && Character.isLetter(cha) )
-                    message3 += (char)((int)message2.charAt(ind)+codeWord[ctr]);
-               else if (!enc && Character.isLetter(cha) )
-                    message3 += (char)((int)message2.charAt(ind)-codeWord[ctr]);
-               else
-                    message3 += message2.charAt(ind);
-               ctr++;   
-               if (ctr >= codeWord.length) ctr = 0;
-            }
-            return message3;
-    }
-        
-    
-   /**
-    *  Returns a capitalized copy of a string.
-    *  @param str the string that is to be output in capitalized form
-    */
-   static String Capitalize( String str ) {
-      char ch;       // One of the characters in str.
-      char prevCh;   // The character that comes before ch in the string.
-      int i;         // A position in str, from 0 to str.length()-1.
-      String cap = "";
-      prevCh = '.';  // Prime the loop with any non-letter character.
-      for ( i = 0;  i < str.length();  i++ ) {
-         ch = str.charAt(i);
-         if ( Character.isLetter(ch) )
-            cap+=( Character.toUpperCase(ch) );
-         else
-            cap+=( ch );
-         prevCh = ch;  // prevCh for next iteration is ch.
-      }
-      return cap;
-   }
-  
-} // end Encoder
+	private File inputFile;
+	
+	public Encoder(String fileName) {
+		inputFile = new File(fileName);
+	}
+	
+	public void Write(String inputString) throws FileNotFoundException, IOException, UnsupportedEncodingException
+	{
+        OutputStream outputStream = new FileOutputStream( inputFile );
+        OutputStream wrappedOS = Base64.getEncoder().wrap( outputStream );
+        wrappedOS.write( inputString.getBytes( "utf-8" ) );
+	}
+	
+	public ArrayList<String> Read() throws FileNotFoundException, IOException, UnsupportedEncodingException
+	{
+		ArrayList<String> outStrings = new ArrayList<String>();
+        InputStream inputStream = new FileInputStream( inputFile );
+        InputStream unWrappedIS = Base64.getDecoder().wrap( inputStream );
+        BufferedReader reader = new BufferedReader(new InputStreamReader(unWrappedIS));
+        String line;
+        while ((line = reader.readLine()) != null) {
+        	outStrings.add(line);
+        }
+		return outStrings;
+	}
+}
