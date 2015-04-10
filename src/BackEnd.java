241799abe36a08f777bf1b0e5adcc5c3288aa259
jacksongray <jacksoncharlesgray@gmail.com>
2015-04-10 14:55:57 -0500

    Nate's changes

--- src/BackEnd.java
+++ src/BackEnd.java
@@ -1,3 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -8,12 +14,22 @@ public class BackEnd
 	
 	public List<String> getMessages(String username)
 	{
-		return null;
+		List<String> messages = new ArrayList<String>();
+		File file = new File(username);
+		try 
+		{
+			FileReader fileReader = new FileReader(file);
+		} 
+		catch (FileNotFoundException e) {}
+		
+		
+		return messages;
 	}
 	
 	public void addMessage(String username, String msg)
 	{
-		
+		Encoder encoder(username);
+		encoder.write(msg);
 	}
 
 	public List<String> getUsers()
